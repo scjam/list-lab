f171f3e48c578f16a60455083b734e635db5248f
Sydney James <sydneyjames7@gmail.com>
2021-02-08 15:28:50 -0800

    fetch api service complete

---
+++ src/services/animalCrossingApi.js
@@ -0,0 +1,27 @@
+export const findVillager = () => {
+  return fetch('https://ac-vill.herokuapp.com/villagers')
+    .then((res) => res.json())
+    .then((villagers) => 
+      villagers.map(({ id, name, image, quote, birthday }) => ({
+        id,
+        name,
+        image,
+        birthday,
+        text: quote
+      }))
+    );
+};
+
+export const findVillagerById = (id) => {
+  return fetch(`https://ac-vill.herokuapp.com/villagers/${id}`)
+    .then((res) => res.jason())
+    .then((villagers) =>
+      villagers.map(({ id, name, image, quote, birthday }) => ({
+        id,
+        name,
+        image,
+        birthday,
+        text: quote
+      }))
+    );
+};
